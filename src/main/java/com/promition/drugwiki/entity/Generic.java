04d6ee3901fca008d6c6a45171dd035ce230ff5d
Hell4U <18BCE131@nirmauni.ac.in>
2021-05-13 22:09:18 +0530

    This is the Generic entity class it have foreign key with Ingredients as the mapping one.

---
+++ src/main/java/com/promition/drugwiki/entity/Generic.java
@@ -0,0 +1,25 @@
+package com.promition.drugwiki.entity;
+
+import com.promition.drugwiki.constant.DosageUnit;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "generics")
+@Data
+public class Generic extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Ingredient ingredient;
+
+    @Column(nullable = false)
+    private Double dosage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable =false)
+    private DosageUnit dosageUnit;
+}
