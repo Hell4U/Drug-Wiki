48bcc8b16bbf015692a44f0e1c1d3b8790a6c63d
Hell4U <18BCE131@nirmauni.ac.in>
2021-05-13 22:07:42 +0530

    This is the main class for running the application.

---
+++ src/main/java/com/promition/drugwiki/DrugWikiApplication.java
@@ -0,0 +1,13 @@
+package com.promition.drugwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugWikiApplication.class, args);
+    }
+
+}
